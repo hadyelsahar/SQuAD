4ab4d604edc5f1ea8bd9f8b61e7d82d58b68b64d
hadyelsahar <hadyelsahar@gmail.com>
2017-01-31 16:37:04 +0100

    train and test on the whole  file

--- main.py
+++ main.py
@@ -8,15 +8,13 @@ https://github.com/danqi/rc-cnn-dailymail/blob/master/code/nn_layers.py#L102
 import numpy as np
 np.random.seed(1337)  # for reproducibility
 
-from keras.preprocessing.text import Tokenizer
 from keras.preprocessing import sequence
 from keras.models import Sequential
 from keras.optimizers import SGD
-from keras.layers import Dense, Dropout, Embedding, LSTM, Bidirectional, Merge, InputLayer
+from keras.layers import Dense, Embedding, LSTM, Bidirectional
 
-from modules.attention import BilinearAttentionLayer, DotproductAttentionLayer
+from modules.attention import BilinearAttentionLayer
 from utils.datareader import SquadReader
-from keras.models import Model
 
 TRAIN_PATH = "./data/train-v1.1.json"
 TEST_PATH = "./data/dev-v1.1.json"
@@ -33,39 +31,36 @@ reader.fit(train[0] + train[1] + test[0] + test[1])
 
 
 P_train, Q_train, Aindx_train, A_train = reader.transform(train)
-# P_test, Q_test, Aindx_test, A_test = reader.transform(test)
+P_test, Q_test, Aindx_test, A_test = reader.transform(test)
 
 # preprocessing
 
 P_train = sequence.pad_sequences(P_train)
 Q_train = sequence.pad_sequences(Q_train)
-# P_test = sequence.pad_sequences(P_test)
+P_test = sequence.pad_sequences(P_test)
 
 MAX_SEQ_WORD_LENGTH = len(P_train[0])
 MAX_Q_WORD_LENGTH = len(Q_train[0])
 
 # hack because answers index sometimes can't match the tokenizer
 # todo : fix
-P_train = np.array([i for c, i in enumerate(P_train) if Aindx_train[c] is not None])[0:100]
-# P_test = [i for c, i in enumerate(P_test) if Aindx_test[c] is not None]
-Q_train = np.array([i for c, i in enumerate(Q_train) if Aindx_train[c] is not None])[0:100]
-# Q_test = [i for c, i in enumerate(Q_test) if Aindx_test[c] is not None]
-A_train = np.array([i for c, i in enumerate(A_train) if Aindx_train[c] is not None])[0:100]
-# A_test = [i for c, i in enumerate(A_test) if Aindx_test[c] is not None]
-Aindx_train = np.array([i for c, i in enumerate(Aindx_train) if Aindx_train[c] is not None])[0:100]
-# Aindx_test = [i for c, i in enumerate(Aindx_test) if Aindx_test[c] is not None]
+P_train = np.array([i for c, i in enumerate(P_train) if Aindx_train[c] is not None])
+P_test = [i for c, i in enumerate(P_test) if Aindx_test[c] is not None]
+Q_train = np.array([i for c, i in enumerate(Q_train) if Aindx_train[c] is not None])
+Q_test = [i for c, i in enumerate(Q_test) if Aindx_test[c] is not None]
+A_train = np.array([i for c, i in enumerate(A_train) if Aindx_train[c] is not None])
+A_test = [i for c, i in enumerate(A_test) if Aindx_test[c] is not None]
+Aindx_train = np.array([i for c, i in enumerate(Aindx_train) if Aindx_train[c] is not None])
+Aindx_test = np.array([i for c, i in enumerate(Aindx_test) if Aindx_test[c] is not None])
 
 # create y as one hot vector
 y_train = np.zeros((Aindx_train.shape[0], MAX_SEQ_WORD_LENGTH))
-
 for c, i in enumerate(Aindx_train):
      y_train[c, i] = 1
 
-# y_test = []
-# for i in Aindx_test:
-#     o = [0] * MAX_SEQ_WORD_LENGTH
-#     o[i] = 1
-#     y_test.append(o)
+y_test = np.zeros((Aindx_test.shape[0], MAX_SEQ_WORD_LENGTH))
+for c, i in enumerate(Aindx_train):
+     y_test[c, i] = 1
 
 
 P_model = Sequential()
@@ -84,4 +79,5 @@ model.add(Dense(MAX_SEQ_WORD_LENGTH, activation='softmax'))
 model.compile(loss='categorical_crossentropy', optimizer=SGD(lr=0.1, clipnorm=10), metrics=['accuracy'])
 model.fit([P_train, Q_train], y_train, nb_epoch=N_EPOCHS, batch_size=64)
 
-#todo : complete the model with start and end
+scores = model.evaluate([P_test, Q_test], y_test)
+print("\n Model Accuracy: %.2f%%" % (scores[1]*100))
