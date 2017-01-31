49b463af30a08ef73e9ebbae2fa86dd722ba15ce
hadyelsahar <hadyelsahar@gmail.com>
2017-01-31 16:23:20 +0100

    fix bugs in attention layers

--- modules/attention.py
+++ modules/attention.py
@@ -14,40 +14,47 @@ from keras.engine.topology import Layer
 from keras.initializations import uniform
 from keras.layers import Merge
 
+from IPython.core.debugger import Pdb; debug_here = Pdb().set_trace
+
+
 class BilinearAttentionLayer(Layer):
 
-    def __init__(self, **kwargs):
+    def __init__(self, layers=None, init='uniform', node_indices=None,
+                 tensor_indices=None, name=None, **kwargs):
         """
         :param init: weight initialization function. Can be the name of an existing function (str),
                 or a Theano function (see: https://keras.io/initializations/).
         :param kwargs:
         """
 
-        # uniform (-0.01,0.01) are the values given in Chen et al. 2016 paper
-        # if init is None:
-        def my_init(shape, name=None):
-            value = uniform(shape, scale=0.01)
-            return K.variable(value, name=name)
+        # layer variables
+        self.init = init
+        self.layers = layers
 
-        self.init = my_init
+        # Layer parameters.
+        self.inbound_nodes = []
+        self.outbound_nodes = []
+        self.constraints = {}
+        self.trainable_weights = []
+        self.non_trainable_weights = []
+        self.supports_masking = False
+        self.uses_learning_phase = False
+        self.input_spec = None  # Compatible with anything.
+        if not name:
+            prefix = self.__class__.__name__.lower()
+            name = prefix + '_' + str(K.get_uid(prefix))
+        self.name = name
 
-        super(BilinearAttentionLayer, self).__init__(**kwargs)
+        self.built = False
+        self.add_inbound_node(layers)
 
     def build(self, input_shape):
-        """
-        :param input_shape:
-                input_shape:[0] : batch * len * h
-                input_shape:[1] : batch * h
-        :return:
-        """
-
         # Create a trainable weight variable for this layer.
         # W = h x h
-        self.W = self.add_weight(shape=(input_shape[-1], input_shape[-1]),
-                                 initializer=self.init,
-                                 trainable=True)
-
-        super(BilinearAttentionLayer, self).build(input_shape)  # Be sure to call this somewhere!
+        # assume it has only one output i.e. node indices = 0
+        h = input_shape[0][-1]
+        self.W = self.add_weight(shape=(h, h), initializer=self.init, trainable=True)
+        self.built = True
 
     def call(self, inputs,  mask=None):
         """
@@ -68,21 +75,9 @@ class BilinearAttentionLayer(Layer):
 
     def get_output_shape_for(self, input_shape):
 
-        return input_shape[0], input_shape[-1]
+        return input_shape[0][0], input_shape[0][-1]
 
-class DotproductAttentionLayer(Layer):
-    def __init__(self, **kwargs):
-
-        super(DotproductAttentionLayer, self).__init__(**kwargs)
-
-    def build(self, input_shape):
-        """
-        :param input_shape:
-                input_shape:[0] : batch * len * h
-                input_shape:[1] : batch * h
-        :return:
-        """
-        super(DotproductAttentionLayer, self).build()  # Be sure to call this somewhere!
+class DotproductAttentionLayer(Merge):
 
     def call(self, inputs, mask=None):
         """
@@ -92,14 +87,18 @@ class DotproductAttentionLayer(Layer):
         :param mask:
         :return:
         """
-        if len(inputs) != 2:
-            raise TypeError('Attention layers must be called on with a list of 2 tensors ' + str(inputs))
 
-        alpha = K.softmax(K.sum(input[0] * inputs[1].dimshuffle(0, 'x', 1), axis=2))
+        a = K.sum(inputs[0] * inputs[1].dimshuffle(0, 'x', 1), axis=2)
+        alpha = K.softmax(a)
         output = K.sum(inputs[0] * alpha.dimshuffle(0, 1, 'x'), axis=1)
 
         return output  # size Batch * h
 
+    def _arguments_validation(self, layers, mode, concat_axis, dot_axes,
+                              node_indices, tensor_indices):
+        return True
+
     def get_output_shape_for(self, input_shape):
-        return input_shape[0], input_shape[-1]
+
+        return input_shape[0][0], input_shape[0][-1]
 
